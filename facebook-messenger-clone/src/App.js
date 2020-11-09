1b455a6a90f6d8075024d3adaf38e9e6d8b37bf3
Asghar-Ali-Mowlana <alimowlana24@gmail.com>
2020-11-09 20:54:41 +0530

    Material UI (Differentiate User & Guest Messages )

--- facebook-messenger-clone/src/App.js
+++ facebook-messenger-clone/src/App.js
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
+import Message from './Message';
 
 function App() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState('');
+  
+  useEffect(() => {
 
-  console.log(input);
-  console.log(messages);
+    setUsername(prompt('Please enter your name'))
+
+  }, [])
 
   const sendMessage = (Event) => {
     Event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, {username: username, text: input}]);
     setInput('');
   }
 
   return (
     <div className="App">
       <h1>Facebook Messenger Clone</h1>
+      <h2>Welcome {username}</h2>  
 
       <form>
-        <input value={input} onChange={Event => setInput(Event.target.value)} />
-        <button type='submit' onClick={sendMessage}>Send Message</button>
-        {/* Input Field */}
-        {/* Buttons */}
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input value={input} onChange={Event => setInput(Event.target.value)} />
+          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+        </FormControl>
       </form>
 
 
@@ -30,7 +38,7 @@ function App() {
 
       {
         messages.map(message => (
-          <p>{message}</p>
+          <Message username={username} message={message} />
         ))
       }
 
